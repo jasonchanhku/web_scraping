bdef1c0fccec5a1d555e16671f37662ca161c3e1
Jason Chan <jasonchanhku@gmail.com>
2017-12-14 22:24:55 +0800

    first spider

--- scrapy/First_Spider.py
+++ scrapy/First_Spider.py
@@ -1,2 +1,22 @@
-print('Creating my first Spider')
+# Multiple pages extraction
 
+# Problem is that multiple quotes are from different quote dividers
+
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    allowed_domains = ['toscrape.com']
+    start_urls = ['http://quotes.toscrape.com']
+
+    def parse(self, response):
+        self.log('I just visited: ' + response.url)
+        for quotes in response.css('.quote'):
+            item = {
+                'author': quotes.css('.author::text').extract(),
+                'text': quotes.css('.text::text').extract(),
+                'tags': quotes.css('.tag::text').extract()
+            }
+            yield item
